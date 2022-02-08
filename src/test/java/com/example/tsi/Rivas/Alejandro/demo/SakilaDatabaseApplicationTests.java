c272d5bd4e9f2dcde62619749bc431b3626fb9d7
AlejandroRT5 <alejandrorivastorres@live.co.uk>
2022-02-08 15:14:26 +0000

    First commit

---
+++ src/test/java/com/example/tsi/Rivas/Alejandro/demo/SakilaDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsi.Rivas.Alejandro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
