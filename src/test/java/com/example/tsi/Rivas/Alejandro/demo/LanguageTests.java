7ff09cc45020592afd02df92caca1d07e2366697
AlejandroRT5 <alejandrorivastorres@live.co.uk>
2022-02-09 11:32:18 +0000

    Edited tests to rebuild

--- src/test/java/com/example/tsi/Rivas/Alejandro/demo/LanguageTests.java
+++ src/test/java/com/example/tsi/Rivas/Alejandro/demo/LanguageTests.java
@@ -12,11 +12,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
             testName.setName("Mandarin");
             assertEquals(testName.getName(),"Mandarin", "Language name should be Mandarin. Error in getName function.");
         }
-        //@Test
-        //void getLanguage_idTest(){
-        //    Language German = new Language("German");
-         //   assertEquals(German.getLanguage_id(), 6, "Language id function not accessing the id number corresponding to the given language German.");
-        //}
 
     }
 
